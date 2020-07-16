21f54dc715b9171f64f577bd5e212a7098775a50
Scorpioeric-dev <ericscorpio1@gmail.com>
2020-07-15 21:37:36 -0600

    string-methods

---
+++ Objects/string-method.js
@@ -0,0 +1,30 @@
+let name = "Eric Fonseca";
+//length property
+console.log(name.length);
+// console.log(name)
+//convert to upper case
+// console.log(name.toUpperCase())
+//convert to lower case
+// console.log(name.toLowerCase())
+
+//Include method
+// let password = "123456789abc";
+// console.log(password.includes("abcd"));
+//returns boolean of false or true
+
+// Trim
+// console.log(name.trim())
+
+//Is valid password
+// length is more than 8 - and it doesn't contain the word password
+let password = "jeremy";
+isValidPasssword = (password) => {
+  return password.length > 8 && password.includes("password");
+};
+
+console.log(isValidPasssword("ddddt")); //false
+
+console.log(isValidPasssword("asldfsdfsaklhflkshd")); 
+
+
+console.log(isValidPasssword("kbsdkbaskdfbspassword")); //false
