25c05ef6dd0b2cddf6fe375dde0950a5a5a15b61
Scorpioeric-dev <ericscorpio1@gmail.com>
2020-07-16 20:35:13 -0600

    num-methods

---
+++ Objects/number-method.js
@@ -0,0 +1,28 @@
+let num = 120.166;
+
+// console.log(num.toFixed(2))
+
+// console.log(Math.round(num))
+//120
+// console.log(Math.floor(num))
+//120
+
+// console.log(Math.ceil(num))
+//121
+let min = 0;
+let max = 20;
+let randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+// 10-20
+console.log(randomNum);
+
+// Challenge guess random
+// 1-5 - true if correct - false if not correct
+
+makeGuess = (guess) => {
+    let min = 1;
+    let max = 5;
+    let randomNum = Math.floor(Math.random() * (max - min + 1)) + min
+    return guess === randomNum
+};
+
+console.log(makeGuess(2));
--- Objects/string-method.js
+++ Objects/string-method.js
@@ -1,30 +1,31 @@
 let name = "Eric Fonseca";
 //length property
-console.log(name.length);
-// console.log(name)
+console.log(name.length);//12
+console.log(name)
+
 //convert to upper case
-// console.log(name.toUpperCase())
+console.log(name.toUpperCase())
 //convert to lower case
-// console.log(name.toLowerCase())
+console.log(name.toLowerCase())
 
 //Include method
-// let password = "123456789abc";
-// console.log(password.includes("abcd"));
+let password = "123456789abc";
+console.log(password.includes("abcd")); //false
 //returns boolean of false or true
 
 // Trim
-// console.log(name.trim())
+console.log(name.trim())
 
 //Is valid password
 // length is more than 8 - and it doesn't contain the word password
-let password = "jeremy";
+// let password = "jeremy";
 isValidPasssword = (password) => {
   return password.length > 8 && password.includes("password");
 };
 
 console.log(isValidPasssword("ddddt")); //false
 
-console.log(isValidPasssword("asldfsdfsaklhflkshd")); 
+console.log(isValidPasssword("asldfsdfsaklhflkshd")); //false
 
 
-console.log(isValidPasssword("kbsdkbaskdfbspassword")); //false
+console.log(isValidPasssword("kbsdkbaskdfbspassword")); //true
