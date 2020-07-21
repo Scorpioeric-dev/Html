b647db8f913d3b393641be09b538165f05577e57
Scorpioeric-dev <ericscorpio1@gmail.com>
2020-07-20 21:21:25 -0600

    let/const

--- Objects/const.js
+++ Objects/const.js
@@ -9,8 +9,13 @@ console.log(person.age)
 // person = {}
 let isRaining = {
   rain: `It has been ${person.age} years since I saw my mom.`,
-  age:24,
+  age:24*7,
   name:'Avenger',
   car:'Porsche'
 }
 console.log(`Wow ${isRaining.rain} I'm ${isRaining.age} years old now with a ${isRaining.car} and they call me ${isRaining.name}`)
+
+
+isRaining.name = 'juggle'
+
+console.log(isRaining.name)
