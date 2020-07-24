a801aaef2ef2207530c02266934feb585edd2567
Scorpioeric-dev <ericscorpio1@gmail.com>
2020-07-23 20:42:57 -0600

    forEach

--- Arrays/notes.js
+++ Arrays/notes.js
@@ -1,8 +1,20 @@
 const notes = [
+    'hello',
     'Note 1',
     'Note 2',
     123,
     false
 ]
 
-console.log(notes[notes.length- 2])
+
+
+console.log(notes)
+// notes.splice(1,1, 'hello')
+
+notes[0] = 'New Hello !'
+
+// notes.forEach(function (item,index) {
+    // console.log(index)
+    // console.log(item)
+// })
+// console.log(notes[notes.length- 2])
--- Arrays/todo.js
+++ Arrays/todo.js
@@ -1,16 +1,36 @@
 //5 differnet things to do
 const todo = [
-  "get milk",
-  "shop for tuna",
-  "play xbox",
-  "go dancing",
+  "racecar ",
+  "shop for tuna ",
+  "play xbox ",
+  "go dancing ",
   "workout",
 ];
 
-let newTodo = todo.push('sing')
-// let done = todo.pop()
 
 
-console.log(`I have ${todo.length} items today `)
-console.log(`Todo: ${todo[0]}`)
-console.log(`Todo: ${todo[todo.length-2]}`)
+// console.log(todo[0].split(''))
+// todo.pop()
+// todo.reverse()
+
+// todo.push('sing',
+// 12,'get my freak on','money')
+
+console.log(todo)
+//delete 3rd item
+// todo.splice(2,1)
+//add item on end
+todo.push('new workout')
+//remove first item
+todo.shift()
+console.log(`You have ${todo.length} to do!`)
+todo.forEach(function (todo,index){
+  const num = index + 1
+console.log(`${num}. ${todo}`)
+})
+
+//first item
+//second item
+// console.log(`I have ${todo.length} items today `)
+// console.log(`Todo: ${todo[0]}`)
+// console.log(`Todo: ${todo[todo.length-2]}`)
