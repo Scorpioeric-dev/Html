c707899da0badab006e9e3d66a0b5e166e29740d
Scorpioeric-dev <ericscorpio1@gmail.com>
2020-11-19 21:08:29 -0800

    find

--- Arrays/notes.js
+++ Arrays/notes.js
@@ -1,20 +1,47 @@
 const notes = [
-    'hello',
-    'Note 1',
-    'Note 2',
-    123,
-    false
-]
+  { title: "hello", body: "bye" },
+  { title: "me", body: 34 },
+  { title: "coding", body: "Enchilidas" },
+];
 
-
-
-console.log(notes)
+console.log(notes);
 // notes.splice(1,1, 'hello')
 
-notes[0] = 'New Hello !'
+// notes[0] = "New Hello !";
 
 // notes.forEach(function (item,index) {
-    // console.log(index)
-    // console.log(item)
+// console.log(index)
+// console.log(item)
 // })
 // console.log(notes[notes.length- 2])
+
+//Counting ..1
+// for(let count = 2 ; count >= 0 ; count--){
+// console.log(count)
+// }
+
+// for (let count = notes.length - 1; count >= 0; count--) {
+//   console.log(notes[count]);
+// }
+
+// console.log(notes.indexOf(false));
+
+// the find () methos allows me to target a specific query or search not based on index the lower case method gives me dynamic input useability by my user
+const findNote = function (notes, noteTitle) {
+  return notes.find(function (note, index) {
+    return note.title.toLowerCase() === noteTitle.toLowerCase();
+  });
+};
+
+
+const note = findNote(notes, "ME");
+console.log(note);
+// console.log(notes.length);
+
+//FindIndex does the same
+//findIndex stops when the value is found in the array
+// const index = notes.findIndex(function (note, index) {
+// console.log(note);
+//   return note.faveFood === "Enchilidas";
+// });
+// console.log(index);
--- Arrays/todo.js
+++ Arrays/todo.js
@@ -1,13 +1,29 @@
-//5 differnet things to do
+//5 different things to do
 const todo = [
-  "racecar ",
-  "shop for tuna ",
-  "play xbox ",
-  "go dancing ",
-  "workout",
+  { text: "racecar", complete: true },
+  { text: "shop for tuna", complete: true },
+  { text: "play xbox", complete: true },
+  { text: "go dancing", complete: true },
+  { text: "workout", complete: true },
 ];
 
+// console.log(todo)
+// console.log(todo)
+//Challenge
 
+//1.Convert array to aray of objects -> text,complete
+
+//2.Create function to remove a todo by text value
+const deleteTodo = (todo, todoType) => {
+  const index = todo.findIndex((todos)=> {
+    return todos.text.toLowerCase() === todoType.toLowerCase();
+  });
+  if (index > -1) {
+    todo.splice(index, 1);
+  }
+};
+deleteTodo(todo, "go dancing");
+console.log(todo);
 
 // console.log(todo[0].split(''))
 // todo.pop()
@@ -16,21 +32,29 @@ const todo = [
 // todo.push('sing',
 // 12,'get my freak on','money')
 
-console.log(todo)
+// console.log(todo)
 //delete 3rd item
 // todo.splice(2,1)
 //add item on end
-todo.push('new workout')
+// todo.push('new workout')
 //remove first item
-todo.shift()
-console.log(`You have ${todo.length} to do!`)
-todo.forEach(function (todo,index){
-  const num = index + 1
-console.log(`${num}. ${todo}`)
-})
+// todo.shift()
+// console.log(`You have ${todo.length} to do!`)
+// todo.forEach(function (todo,index){
+//   const num = index + 1
+// console.log(`${num}. ${todo}`)
+// })
 
 //first item
 //second item
 // console.log(`I have ${todo.length} items today `)
 // console.log(`Todo: ${todo[0]}`)
 // console.log(`Todo: ${todo[todo.length-2]}`)
+
+for (let count = 0; count <= 4; count++) {
+  console.log(todo[count])
+}
+
+for (let count = todo.length - 1; count >= 0; count--) {
+  // console.log(todo[count]);
+}
