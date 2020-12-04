a5af96180b71a665172a9cd3796eb17204c43b99
Scorpioeric-dev <ericscorpio1@gmail.com>
2020-12-03 19:23:39 -0800

    eventListener

--- Arrays/notes-app.js
+++ Arrays/notes-app.js
@@ -1,26 +1,39 @@
 //DOM - Document Object Model
 const notes = [
-    { title: "javascript", body: "arrays" },
-    { title: "react", body: "hooks" },
-    { title: "node", body: "server" },
-  ];
+  { title: "javascript", body: "arrays" },
+  { title: "react", body: "hooks" },
+  { title: "node", body: "server" },
+];
+
+document.querySelector("#post_note").addEventListener("click", (e) => {
+  console.log('The button was clicked')
+});
+
+document.querySelector('#remove_notes').addEventListener('click',()=>{
+  document.querySelectorAll('.note').forEach((note) => {
+    note.remove()
+  })
+})
+
+
+
 //Query and remove
 
 // const p = document.querySelector('p')
 // p.remove()
 
 //Query all and remove
-const ps = document.querySelectorAll('p')
-ps.forEach((par)=>{
-    par.textContent = "*****"
-// par.remove()
-})
+// const ps = document.querySelectorAll('p')
+// ps.forEach((par)=>{
+//     par.textContent = "*****"
+// // par.remove()
+// })
 
-//Add a new element 
-const newP = document.createElement('p')
+//Add a new element
+// const newP = document.createElement('p')
 
-newP.textContent = "This is a new element from JS"
+// newP.textContent = "This is a new element from JS"
 
-document.querySelector('body').appendChild(newP)
+// document.querySelector('body').appendChild(newP)
 
-//Update context 
+//Update context
--- Todo-app/html-todo.js
+++ Todo-app/html-todo.js
@@ -7,21 +7,29 @@ const todo = [
 ];
 
 
+//Access the button and makes the code run on console
+document.querySelector("button").addEventListener("click", () => {
+  console.log("Adding a todo item ");
+});
+
+//Challenge target the button by id
+document.querySelector("#post_todo").addEventListener("click", (e) => {
+  console.log('The button to post a todo')
+});
+
 //This filters out the todo with completed false ---
 const incompleteTodos = todo.filter((todo) => {
   return !todo.complete;
 });
 
-
 //this variable creates a html element
 const summary = document.createElement("h2");
 
-//this put's content within the element 
+//this put's content within the element
 summary.textContent = `You have ${incompleteTodos.length} todos  left on your list`;
 
 //This inserts the content into the body of the html
 document.querySelector("body").appendChild(summary);
-
 
 //Takes each object within the array of objects and put it in a <p></p> tag grabbing the text key
 
@@ -32,23 +40,17 @@ todo.forEach((todo)=>{
 
 })
 
-// const par = document.querySelectorAll("p");
-// console.log(par);
+const par = document.querySelectorAll("p");
+console.log(par);
 
 //This allows me to remove an element tag based on the context of it
 //The forEach combined with includes goes through the tags looking for the string I passed into it
 
-// par.forEach((p) => {
-//   if (p.textContent.includes("One")) {
-//     p.remove();
-//   }
-// });
+par.forEach((p) => {
+  if (p.textContent.includes("One")) {
+    p.remove();
+  }
+});
 
 //you have 2 todo left (p)
 //Add a p for each todo above (use text value)
-
-
-
-
-
-
